fa50ad9fed3c4a5ec049f909c6d4646972655fc4
amethyst sanders <asanders35@gatech.edu>
2016-09-23 21:48:26 -0400

    Created the pages for a bad login attempt. These need to be changed later to mirror login.

---
+++ src/controller/badloginController.java
@@ -0,0 +1,14 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import fxapp.MainFXApplication;
+
+
+//make this match the login screen's controller.
+
+public class badloginController {
+
+
+}
