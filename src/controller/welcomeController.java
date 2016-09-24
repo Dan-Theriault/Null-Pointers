e2b21e3562b0a3c44fe62da79bc0533eabddaf52
amethyst sanders <asanders35@gatech.edu>
2016-09-23 21:07:52 -0400

    Made a welcome page and a controller for it. The controller isn't written.

---
+++ src/controller/welcomeController.java
@@ -0,0 +1,20 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import fxapp.MainFXApplication;
+
+
+public class welcomeController {
+    @FXML
+    private Button loginbutton;
+
+    @FXML
+    private Button registerbutton;
+
+    private MainFXApplication mainApplication;
+
+
+
+}
